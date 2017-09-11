33dd3b66a351fce54cda1824a6b331fc112141ad
levsthings <bearnode@gmail.com>
2017-09-12 00:56:37 +0300

    remove unused image

--- src/js/components/Controls.jsx
+++ src/js/components/Controls.jsx
@@ -1,6 +1,5 @@
 import React, {PureComponent} from 'react'
 import PropTypes from 'prop-types'
-// import Fight from '../../img/fight.svg'
 
 export default class Controls extends PureComponent {
     static propTypes = {
